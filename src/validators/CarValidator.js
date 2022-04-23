501a83b825db0cbb627c873d71244df3ba7c2d7c
MykolaTurchyn <tyrchnmikigt@gmail.com>
2022-04-23 20:51:50 +0300

    ///

--- src/validators/CarValidator.js
+++ src/validators/CarValidator.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const carValidator = Joi.object({
     model: Joi.string().min(2).max(20).required(),
-    // regex(/^[a-zA-Zа-яА-Я]{1-20}$/),
+        // regex(/^[a-zA-Zа-яА-Я]{1-20}$/),
     year: Joi.number().min(1990).max(new Date().getFullYear()).required(),
     price: Joi.number().min(1).max(1000000).required()
 });
